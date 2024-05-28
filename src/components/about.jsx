37878940bd61d325eb38f790620486cfed10fefc
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2024-05-28 11:19:37 +0530

    Changed about content in about.js

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -10,11 +10,11 @@ function About() {
             </div>
             <div className="text">
                 <h1>Shiva Krishna Reddy Burra</h1>
-                <p>I am currently pursuing B.Tech at Kakatiya Institute of Technology and Science, Warangal, as a second-year student in the Computer Science Department.
+                <p>I am currently pursuing a B.Tech at Kakatiya Institute of Technology and Science, Warangal, where I am a second-year student in the Computer Science Department. My interest in web development has driven me to gain proficiency in a range of technologies. On the frontend, I have learned HTML, CSS, JavaScript, and React. On the backend, I have learned Node.js, Express.js, and Embedded JavaScript (EJS). Additionally, I have developed skills in working with REST APIs and PostgreSQL, which round out my abilities as a Full Stack Developer.<br></br><br></br>
 
-                    I obtained certifications in the C language and Java through NPTEL. Due to my interest in web development, I learned HTML, CSS, and JavaScript as part of the FrontEnd. Currently, I am focusing on the BackEnd to become a Full Stack Developer.
-                
-                     My hobbies include watching anime and playing games. </p>
+To validate and enhance my knowledge, I have obtained various certifications from respected platforms. I earned certificates in C Language and Java from NPTEL, and in JavaScript, Python, and SQL from HackerRank. I have also completed courses on Udemy to further my understanding of web development.
+
+<br></br><br></br>In my free time, I enjoy watching anime and playing games, which help me relax and stay creative. </p>
             </div>
         </div>
     );
